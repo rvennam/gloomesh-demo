6686d1026c9f9a82f82981662b26f05e55039cef
Ram Vennam <ram.vennam@solo.io>
2025-02-03 15:03:11 -0500

    omni

--- omni/clean.sh
+++ omni/clean.sh
@@ -15,3 +15,9 @@ for context in ${CLUSTER1} ${CLUSTER2}; do
   helm uninstall gloo -n gloo-system --kube-context=${context}
   kubectl --context=${context} delete ns gloo-mesh
 done
+
+for context in ${CLUSTER1} ${CLUSTER2}; do
+  kubectl --context ${context} create ns bookinfo 
+  kubectl --context ${context} apply -n bookinfo -f https://raw.githubusercontent.com/istio/istio/release-1.24/samples/bookinfo/platform/kube/bookinfo.yaml
+  kubectl --context ${context} apply -n bookinfo -f https://raw.githubusercontent.com/istio/istio/release-1.24/samples/bookinfo/platform/kube/bookinfo-versions.yaml
+done
