131decf601b28b78a94347ac16839c66e8746d01
Ram Vennam <ram.vennam@solo.io>
2025-02-05 10:57:26 -0500

    cleanup

--- omni/test.sh
+++ omni/test.sh
@@ -1,34 +1,3 @@
-# Omni
-![alt text](image.png)
-Gloo: An omni-directional solution that covers ingress, service-to-service, and egress traffic with a unified approach.
-
-### Env
-
-1. Create two clusters
-2. Download the latest solo istioctl nightly build
-
-Set env vars
-```bash
-export CLUSTER1=gke_ambient_one
-export CLUSTER2=gke_ambient_two
-export ISTIOCTL=/Users/ramvennam/Downloads/istioctl
-```
-
-## Deploy Bookinfo sample
-
-```bash
-for context in ${CLUSTER1} ${CLUSTER2}; do
-  kubectl --context ${context} create ns bookinfo 
-  kubectl --context ${context} apply -n bookinfo -f https://raw.githubusercontent.com/istio/istio/release-1.24/samples/bookinfo/platform/kube/bookinfo.yaml
-  kubectl --context ${context} apply -n bookinfo -f https://raw.githubusercontent.com/istio/istio/release-1.24/samples/bookinfo/platform/kube/bookinfo-versions.yaml
-done
-```
-
-## Install Gloo Gateway on cluster1
-
-```bash
-helm repo add glooe https://storage.googleapis.com/gloo-ee-helm
-helm repo update
 
 helm upgrade --install -n gloo-system gloo glooe/gloo-ee \
 --create-namespace \
@@ -36,11 +5,7 @@ helm upgrade --install -n gloo-system gloo glooe/gloo-ee \
 --kube-context ${CLUSTER1} \
 --set-string license_key=$GLOO_GATEWAY_LICENSE_KEY \
 -f ./gg-values.yaml
-```
 
-## Gloo Gateway to expose productpage
-
-```yaml
 kubectl --context=${CLUSTER1} apply -f - <<EOF
 kind: Gateway
 apiVersion: gateway.networking.k8s.io/v1
@@ -84,12 +49,7 @@ spec:
     - name: productpage
       port: 9080
 EOF
-```
 
-# Install Istio
-
-## Configure Trust
-```bash
 function create_cacerts_secret() {
   context=${1:?context}
   cluster=${2:?cluster}
@@ -104,19 +64,11 @@ function create_cacerts_secret() {
 
 create_cacerts_secret ${CLUSTER1} cluster1
 create_cacerts_secret ${CLUSTER2} cluster2
-```
 
-## Install the Gloo Operator
-```bash
 for context in ${CLUSTER1} ${CLUSTER2}; do
   helm upgrade --install --kube-context=${context} gloo-operator oci://us-docker.pkg.dev/solo-public/gloo-operator-helm/gloo-operator --version 0.1.0-beta.3 -n gloo-system --create-namespace &
 done
-```
 
-
-Use the `ServiceMeshController` resource to install Istio on both clusters
-
-```bash
 kubectl --context=${CLUSTER1} apply -f - <<EOF
 apiVersion: operator.gloo.solo.io/v1
 kind: ServiceMeshController
@@ -148,45 +100,25 @@ spec:
   image:
     repository: docker.io/rvennam
 EOF
-```
 
+sleep 15
 
-
-## Onboard Gloo Gateway and Bookinfo to the Mesh
-
-```bash
 kubectl --context ${CLUSTER1} label namespace gloo-system istio.io/dataplane-mode=ambient
 kubectl --context ${CLUSTER1} label namespace bookinfo istio.io/dataplane-mode=ambient
 kubectl --context ${CLUSTER2} label namespace bookinfo istio.io/dataplane-mode=ambient
-```
 
-### Peer the clusters together
-
-Deploy an east-west gateway:
-```bash
 $ISTIOCTL --context=${CLUSTER1} multicluster expose -n istio-gateways
 $ISTIOCTL --context=${CLUSTER2} multicluster expose -n istio-gateways
-```
 
-Link clusters together:
-```bash
+sleep 30
 $ISTIOCTL multicluster link --contexts=$CLUSTER1,$CLUSTER2 -n istio-gateways
-```
 
-## Multi-Cluster Services
-
-Enable productpage to be multi-cluster on both clusters
-```bash
 for context in ${CLUSTER1} ${CLUSTER2}; do
   kubectl --context ${context}  -n bookinfo label service productpage solo.io/service-scope=global
   kubectl --context ${context}  -n bookinfo annotate service productpage  networking.istio.io/traffic-distribution=Any
 done
-```
 
-
-## Gloo Gateway Multi-Cluster Routing
-Update the previously applied HTTPRoute to route to the global productpage destination:
-```yaml
+kubectl --context=${CLUSTER1} apply -f - <<EOF
 apiVersion: gateway.networking.k8s.io/v1
 kind: HTTPRoute
 metadata:
@@ -218,11 +150,9 @@ spec:
       group: networking.istio.io
       name: productpage.bookinfo.mesh.internal
       port: 9080
-```
+EOF
 
-## Gloo Gateway as Waypoint
-
-```yaml
+kubectl --context=${CLUSTER1} apply -f - <<EOF
 apiVersion: gateway.networking.k8s.io/v1
 kind: Gateway
 metadata:
@@ -234,19 +164,14 @@ spec:
  - name: proxy
    port: 15088
    protocol: istio.io/PROXY
-```
-```bash
-kubectl --context ${CLUSTER1} label ns bookinfo istio.io/use-waypoint=gloo-waypoint  --overwrite
-```
+EOF
 
-Use standard waypoint in cluster2
-```bash
+kubectl --context ${CLUSTER1} label ns bookinfo istio.io/use-waypoint=gloo-waypoint  --overwrite
+
 $ISTIOCTL --context=${CLUSTER2} waypoint apply -n bookinfo
 kubectl --context ${CLUSTER2} label ns bookinfo istio.io/use-waypoint=waypoint  --overwrite
-```
 
-Use waypoints for canary traffic going to reviews
-```yaml
+kubectl --context=${CLUSTER1} apply -f - <<EOF
 apiVersion: gateway.networking.k8s.io/v1
 kind: HTTPRoute
 metadata:
@@ -269,16 +194,12 @@ spec:
   - backendRefs:
     - name: reviews-v1
       port: 9080
-```
-Apply the above yaml to both clusters:
-```bash
+EOF
+
 kubectl apply -f reviews-L7.yaml --context $CLUSTER1
 kubectl apply -f reviews-L7.yaml --context $CLUSTER2
-```
 
-## Gloo Gateway as an AI Gateway
-
-```yaml
+kubectl --context=${CLUSTER1} apply -f - <<EOF
 apiVersion: gateway.gloo.solo.io/v1alpha1
 kind: GatewayParameters
 metadata:
@@ -312,15 +233,13 @@ spec:
     allowedRoutes:
       namespaces:
         from: All
-```
+EOF
 
-```bash
 kubectl --context ${CLUSTER1} create secret generic openai-secret -n gloo-system \
 --from-literal="Authorization=Bearer $OPENAI_API_KEY" \
 --dry-run=client -oyaml | kubectl apply -f -
-```
 
-```yaml
+kubectl --context=${CLUSTER1} apply -f - <<EOF
 apiVersion: gloo.solo.io/v1
 kind: Upstream
 metadata:
@@ -355,35 +274,19 @@ spec:
       namespace: gloo-system
       group: gloo.solo.io
       kind: Upstream
-```
-
-Call OpenAI from ratings:
-```bash
-kubectl --context ${CLUSTER1} exec -n bookinfo deploy/ratings-v1 -c ratings -- curl -v "gloo-proxy-ai-gateway.gloo-system:8080/openai" -H content-type:application/json -d '{"model": "gpt-4o-mini","max_tokens": 128,"messages": [{"role": "system","content": "What is an Omni Gateway?"}]}' | jq
-```
-
-## Gloo Management Plane
-
-### cluster1 will be both workload and managment:
-```bash
+EOF
 
 helm upgrade -i gloo-platform-crds gloo-platform/gloo-platform-crds -n gloo-mesh --create-namespace --version=2.7.0-beta1
 helm upgrade -i gloo-platform gloo-platform/gloo-platform -n gloo-mesh --version 2.7.0-beta1 --values mgmt-values.yaml \
   --set licensing.glooMeshLicenseKey=$GLOO_MESH_LICENSE_KEY
-```
 
-### Register cluster2 as a workload cluster to cluster1:
-```bash
+
+
+sleep 60
 export TELEMETRY_GATEWAY_ADDRESS=$(kubectl get svc -n gloo-mesh gloo-telemetry-gateway --context $CLUSTER1 -o jsonpath="{.status.loadBalancer.ingress[0]['hostname','ip']}"):4317
 
 meshctl cluster register cluster2  --kubecontext $CLUSTER1 --profiles gloo-core-agent --remote-context $CLUSTER2 --telemetry-server-address $TELEMETRY_GATEWAY_ADDRESS
-```
 
-To see the new graph, update the ui image:
-```bash
 k apply -f ./gloo-mesh-ui-gloo-mesh-cluster-role.yaml
 k  set image Deployment/gloo-mesh-ui -n gloo-mesh console=us-docker.pkg.dev/developers-369321/gloo-platform-dev/gloo-mesh-ui:2.7.0-beta1-2025-01-16-arek-graph-ec733223b6
 k set image Deployment/gloo-mesh-ui -n gloo-mesh gloo-mesh-ui=us-docker.pkg.dev/developers-369321/gloo-platform-dev/gloo-mesh-apiserver:2.7.0-beta1-2025-01-16-arek-graph-ec733223b6
-```
-
-![alt text](ui-screenshot.png)
